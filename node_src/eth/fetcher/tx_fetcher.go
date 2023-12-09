fdf83dd25df5286f282a44e983ece1c09382fae7
hide001 <shubhamswaraj1@gmail.com>
2023-12-09 23:07:01 +0530

    patch-6 'TXF-01 | Underpriced Transactions Should Be Rebroadcasted After Timeout'

--- node_src/eth/fetcher/tx_fetcher.go
+++ node_src/eth/fetcher/tx_fetcher.go
@@ -26,7 +26,7 @@ import (
 
 	
 	"github.com/ethereum/go-ethereum/common"
-	lru "github.com/hashicorp/golang-lru"
+	"github.com/ethereum/go-ethereum/common/lru"
 	"github.com/ethereum/go-ethereum/common/mclock"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
